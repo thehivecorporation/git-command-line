4b3162d888584cdd293eb4f4cb44755b51b57479
fab1o <fab1o@users.noreply.github.com>
2021-09-15 23:13:07 -0400

    Fix prepareExecOptions

--- src/prepareExecOptions.js
+++ src/prepareExecOptions.js
@@ -5,13 +5,8 @@
  * @returns {*}
  */
  module.exports = function prepareExecOptions(execOptions, workingDirectory) {
-    if (!execOptions) {
-        execOptions = {
-            cwd: workingDirectory
-        };
-    } else if (!execOptions.cwd) {
-        execOptions.cwd = workingDirectory;
-    }
+    execOptions = execOptions || {};
+    execOptions.cwd = execOptions.cwd || workingDirectory;
 
     return execOptions;
 }
